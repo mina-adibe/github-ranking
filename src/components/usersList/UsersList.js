2c057f91336146f661529cadd8b2b4c581c913ff
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-15 12:51:14 +0200

    get and display data

--- src/components/usersList/UsersList.js
+++ src/components/usersList/UsersList.js
@@ -1,15 +1,33 @@
-import React from "react";
-import styles from "./UsersList.module.css";
-import User from "../user/User";
+import React, { useEffect, useState } from "react";
+//import styles from "./UsersList.module.css";
+import Repo from "../repo/Repo";
+import { getRepos } from "../../api/repoApi";
 
 const UsersList = () => {
+  const [repos, setRepos] = useState(null);
+
+  useEffect(() => {
+    const getdata = () => {
+      getRepos().then((res) => {
+        setRepos(res.items);
+      });
+    };
+    getdata();
+  }, []);
+
   return (
     <div>
-      <h1>list</h1>
-      <User />
-      <User />
-      <User />
-      <User />
+      {repos &&
+        repos.map((elm) => (
+          <Repo
+            name={elm.name}
+            description={elm.description}
+            stars={elm.stargazers_count}
+            issues={elm.open_issues_count}
+            Username={elm.owner.login}
+            avatar={elm.owner.avatar_url}
+          />
+        ))}
     </div>
   );
 };
