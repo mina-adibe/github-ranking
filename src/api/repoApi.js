f71abe3f77b8fb7fb7840cbef0427f819c17a0dc
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-15 22:20:20 +0200

    add parameter to getRepos function

--- src/api/repoApi.js
+++ src/api/repoApi.js
@@ -1,9 +1,11 @@
 import { handleResponse, handleError } from "./apiUtils";
 
-const baseUrl =
-  process.env.REACT_APP_API_URL +
-  "search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=1";
+const baseUrl = process.env.REACT_APP_API_URL;
 
-export function getRepos() {
-  return fetch(baseUrl).then(handleResponse).catch(handleError);
+export function getRepos(page) {
+  return fetch(
+    `${baseUrl}search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}`
+  )
+    .then(handleResponse)
+    .catch(handleError);
 }
