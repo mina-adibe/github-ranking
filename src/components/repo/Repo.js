54b7ef7fe7243fd80feb9d6e7bc7a7062789745f
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-15 22:17:54 +0200

    add prop Types to repo component

--- src/components/repo/Repo.js
+++ src/components/repo/Repo.js
@@ -1,22 +1,32 @@
 import React from "react";
-//import styles from "./Repo";
+import styles from "./Repo.module.css";
+import PropTypes from "prop-types";
 
 const Repo = ({ name, description, stars, issues, Username, avatar }) => {
   return (
-    <div className="container">
-      <div></div>
+    <div className={styles.container}>
       <div>
-        <h2>Repo Names : {name}</h2>
-        <p>Repo description {description}</p>
-        <p>stars : {stars} </p>
-        <p>issues : {issues} </p>
-        <p>Username : {Username} </p>
+        <img src={avatar} alt={Username} className={styles.image} />
+      </div>
+      <div>
+        <h2> {name}</h2>
+        <p> {description}</p>
         <div>
-          <img src={avatar} alt="Logo" />
+          <p>stars: {stars} </p>
+          <p>issues: {issues} </p>
+          <p>Username: {Username} </p>
         </div>
       </div>
     </div>
   );
+};
+Repo.propTypes = {
+  name: PropTypes.string,
+  description: PropTypes.string,
+  stars: PropTypes.number,
+  issues: PropTypes.number,
+  Username: PropTypes.string,
+  avatar: PropTypes.string,
 };
 
 export default Repo;
