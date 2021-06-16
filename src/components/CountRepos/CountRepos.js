764b47cdcb29ad5cdc1e5e68236f2c40418bb546
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-16 22:42:54 +0200

    add style

--- src/components/CountRepos/CountRepos.js
+++ src/components/CountRepos/CountRepos.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./CountRepos.module.css";
 
-const CountRepos = ({ countrepos }) => {
+const CountRepos = ({ countrepos, page }) => {
   return (
     <div className={styles.container}>
-      <h2>number of repos : {countrepos}</h2>
+      <h3>page: {page}</h3>
+      <h3>No of repos: {countrepos}</h3>
     </div>
   );
 };
