b272a971528b48fc19534ec449a2c9f21a808589
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-16 22:43:55 +0200

    adds style to repoDetails component

--- src/components/RepoDetails/RepoDetails.js
+++ src/components/RepoDetails/RepoDetails.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./RepoDetails.module.css";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { AiOutlineStar } from "react-icons/ai";
 
 const RepoDetails = ({
   name,
@@ -13,7 +14,7 @@ const RepoDetails = ({
 }) => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.container_left}>
         <img
           loading="lazy"
           src={avatar}
@@ -21,25 +22,38 @@ const RepoDetails = ({
           className={styles.image}
         />
       </div>
-      <div>
-        <h2>
-          <Link to={`/repo/${username}/${name}`}>{name}</Link>
-        </h2>
-        <p> {description}</p>
+      <div className={styles.container_right}>
         <div>
-          <p>stars: {stars} </p>
-          <p>issues: {issues} </p>
+          <h2>
+            <Link to={`/repo/${username}/${name}`}>{name}</Link>
+          </h2>
+        </div>
+
+        <div className={styles.title}>
           <p>Username: {username} </p>
+        </div>
+        <div className={styles.title}>
+          <p>Description: {description}</p>
+        </div>
+
+        <div>
+          <div className={styles.my_button}>
+            <span style={{ fontSize: "20.5px" }}>
+              <AiOutlineStar />
+            </span>
+            stars: {stars}
+          </div>
+          <div className={styles.my_button}>issues: {issues} </div>
         </div>
       </div>
     </div>
   );
 };
 RepoDetails.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
   description: PropTypes.string,
-  stars: PropTypes.number,
-  issues: PropTypes.number,
+  stars: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  issues: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   Username: PropTypes.string,
   avatar: PropTypes.string,
 };
