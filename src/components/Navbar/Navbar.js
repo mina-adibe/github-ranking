b5dcfe383a2402dfeea809470fb4f949151110d4
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-16 11:05:28 +0200

    add logo and style to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { AiFillGithub } from "react-icons/ai";
 
-const Navbar = () => {
+const Navbar = ({ title }) => {
   return (
     <>
       <div className={styles.nav}>
-        <h1>List of Repos</h1>
+        <div>
+          <Link to="/">
+            <h1>
+              <AiFillGithub /> Github Ranking
+            </h1>
+          </Link>
+        </div>
+        <div>
+          <h1>{title}</h1>
+        </div>
       </div>
     </>
   );
