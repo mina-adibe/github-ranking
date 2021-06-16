e29894178a338791f1d3bb27b7877f2b96010e2e
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-16 07:55:34 +0200

    add navbar

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,2 +1,3 @@
 export { default as RepoDetailes } from "./RepoDetailes/RepoDetailes";
 export { default as ReposList } from "./ReposList/ReposList";
+export { default as Navbar } from "./Navbar/Navbar";
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className={styles.nav}>
+        <h1>List of Repos</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
