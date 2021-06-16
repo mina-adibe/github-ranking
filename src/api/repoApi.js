af1f4c19496dd0fa67f97bbdaf17f3a5afedc20c
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-06-16 20:58:22 +0200

    add moment package

--- src/api/repoApi.js
+++ src/api/repoApi.js
@@ -1,10 +1,13 @@
+import moment from "moment";
 import { handleResponse, handleError } from "./apiUtils";
 
 const baseUrl = process.env.REACT_APP_API_URL;
+//get date 30 days from now
+const DATE_30_DAYS_BEFORE = moment().subtract(30, "days").format("YYYY-MM-DD");
 
 export function getRepos(page) {
   return fetch(
-    `${baseUrl}search/repositories?q=created:>2021-05-16&sort=stars&order=desc&page=${page}`
+    `${baseUrl}search/repositories?q=created:>${DATE_30_DAYS_BEFORE}&sort=stars&order=desc&page=${page}`
   )
     .then(handleResponse)
     .catch(handleError);
